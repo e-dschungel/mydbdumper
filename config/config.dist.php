bba7dc3987d958ad840ff69dd0bdbdc5f39ed7aa
e-dschungel <github@e-dschungel.de>
2022-05-15 12:24:01 +0200

    add default config

---
+++ config/config.dist.php
@@ -0,0 +1,9 @@
+<?php
+
+$config["username"] = "username";
+$config["password"] = "password";
+$config["backupDir"] = "../backup";
+$config["emailFrom"] = "test@example.com";
+$config["emailTo"] = "test@example.com";
+$config["emailBackend"] = "mail";
+$config["maxNrBackups"] = 10000;
